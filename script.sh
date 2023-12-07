bc1977cb880a58d4b9e94f805dc1d2aad3a9575f
Ervalsa Dwi Nanda <ervalsananda@gmail.com>
2023-12-07 22:00:43 +0700

    RESTART

--- script.sh
+++ script.sh
@@ -1,7 +1,11 @@
+docker container logs nama-container
 docker compose create
 docker compose start
 docker compose ps
 docker compose down
+docker compose inspect nama-container
 docker volume ls
 docker volume rm nama-volume
-docker compose inspect nama-container
+docker volume prune
+docker events
+docker events --filter 'container=nama'
