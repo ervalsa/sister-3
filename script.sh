7aec0cee4c26f2b6e5709a2f986565b68613e93a
Ervalsa Dwi Nanda <ervalsananda@gmail.com>
2023-12-07 22:21:52 +0700

    BUILD

--- script.sh
+++ script.sh
@@ -5,8 +5,10 @@ docker compose start
 docker compose ps
 docker compose down
 docker compose inspect nama-container
+docker compose build
 docker volume ls
 docker volume rm nama-volume
 docker volume prune
 docker events
 docker events --filter 'container=nama'
+docker image rm nama-image:tag
